2c4f4fe02a950b800421dca5a3a488b66c675bb9
Tim Moses Server <timmoses-server@moses.com>
2022-01-08 22:58:10 +0000

    Updated example. Fixed config filename in control.py.

--- control.py
+++ control.py
@@ -4,7 +4,7 @@ import time
 
 import pi_control.panel
 
-panel = pi_control.panel.Panel('monitor_panel', '/opt/control/control2.yml', True)
+panel = pi_control.panel.Panel('monitor_panel', '/opt/control/control.yml', True)
 
 # print("{}: {}".format(panel.name, panel.devices))
 # print(panel.devices['power_switch']._actions)
